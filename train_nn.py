bedd964c09993ed5b70c556ea05ce6fe14fec9dd
gjerome1991 <g_kip@live.com>
2018-06-20 22:41:12 -0400

    save model and plot some predictions with labels

--- train_nn.py
+++ train_nn.py
@@ -84,8 +84,38 @@ keras_model= cats_dogs_nn.build_keras()
 tf_model.fit({'input': X_train}, {'targets': Y_train}, n_epoch=3, validation_set=({'input': X_test}, {'targets': Y_test}), 
     snapshot_step=500, show_metric=True, run_id=MODEL_NAME)
 
+tf_model.save(MODEL_NAME)
+
 keras_model.fit(X_train, Y_train,
           batch_size=100,
           epochs=10,
           verbose=1,
           validation_data=(X_test, Y_test))
+
+
+# Plot some predictions on test data using saved model
+
+test_data = np.load('test_data.npy')
+fig=plt.figure()
+
+for num,data in enumerate(test_data[:12]):
+    # cat: [1,0]
+    # dog: [0,1]
+    
+    img_num = data[1]
+    img_data = data[0]
+    
+    y = fig.add_subplot(3,4,num+1)
+    orig = img_data
+    data = img_data.reshape(IMG_SIZE,IMG_SIZE,1)
+    #model_out = model.predict([data])[0]
+    model_out = tf_model.predict([data])[0]
+    
+    if np.argmax(model_out) == 1: str_label='Dog'
+    else: str_label='Cat'
+        
+    y.imshow(orig,cmap='gray')
+    plt.title(str_label)
+    y.axes.get_xaxis().set_visible(False)
+    y.axes.get_yaxis().set_visible(False)
+plt.show()
