44265c9bc80a833f92b3309028dddaad153d0c2c
gjerome1991 <g_kip@live.com>
2018-06-28 21:32:00 -0400

    Do some refactoring and go through training model for 80% accuracy

---
+++ train_tf_nn.py
@@ -0,0 +1,114 @@
+# import the necessary packages
+import numpy
+from keras.datasets import mnist
+from keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_array, load_img
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import Dropout
+from keras.utils import np_utils
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+import cv2
+from random import shuffle
+from tqdm import tqdm
+from model_scripts.cats_dogs import cats_dogs_nn
+
+TRAIN_DIR = '/Users/geoffrey.kip/Projects/cats_vs_dogs_keras/train'
+TEST_DIR = '/Users/geoffrey.kip/Projects/cats_vs_dogs_keras/test'
+IMG_SIZE = 50
+LR = 1e-3
+
+MODEL_NAME = 'dogs_vs_cats_tf_nn' 
+
+def label_img(img):
+    word_label = img.split('.')[-3]
+    print(word_label)
+    # conversion to one-hot array [cat,dog]
+    #                            [much cat, no dog]
+    if word_label == 'cat': return [1,0]
+    #                             [no cat, very doggo]
+    elif word_label == 'dog': return [0,1]
+
+def create_train_data():
+    training_data = []
+    for img in tqdm(os.listdir(TRAIN_DIR)):
+        label = label_img(img)
+        path = os.path.join(TRAIN_DIR,img)
+        img = cv2.imread(path,cv2.IMREAD_GRAYSCALE)
+        img = cv2.resize(img, (IMG_SIZE,IMG_SIZE))
+        training_data.append([np.array(img),np.array(label)])
+    shuffle(training_data)
+    np.save('train_data.npy', training_data)
+    return training_data
+
+def process_test_data():
+    testing_data = []
+    for img in tqdm(os.listdir(TEST_DIR)):
+        path = os.path.join(TEST_DIR,img)
+        img_num = img.split('.')[0]
+        img = cv2.imread(path,cv2.IMREAD_GRAYSCALE)
+        img = cv2.resize(img, (IMG_SIZE,IMG_SIZE))
+        testing_data.append([np.array(img), img_num])
+
+    shuffle(testing_data)
+    np.save('test_data.npy', testing_data)
+    return testing_data
+
+#Show train data cat image
+img = load_img('/Users/geoffrey.kip/Projects/cats_vs_dogs_keras/train/cat.0.jpg')  # this is a PIL image
+img.show()
+
+#train_data = create_train_data()
+#test_data= process_test_data()
+
+train_data = np.load('train_data.npy')
+test_data = np.load('test_data.npy')
+
+train = train_data[:-500]
+test = train_data[-500:]
+
+X_train = np.array([i[0] for i in train]).reshape(-1,IMG_SIZE,IMG_SIZE,1)
+Y_train = [i[1] for i in train]
+
+X_test = np.array([i[0] for i in test]).reshape(-1,IMG_SIZE,IMG_SIZE,1)
+Y_test = [i[1] for i in test]
+
+if os.path.exists('{}.meta'.format(MODEL_NAME)):
+    model.load(MODEL_NAME)
+    print('model loaded!')
+
+tf_model= cats_dogs_nn.build_tf()
+
+tf_model.fit({'input': X_train}, {'targets': Y_train}, n_epoch=3, validation_set=({'input': X_test}, {'targets': Y_test}), 
+    snapshot_step=500, show_metric=True, run_id=MODEL_NAME)
+
+tf_model.save(MODEL_NAME)
+
+
+# Plot some predictions on test data using saved model
+
+test_data = np.load('test_data.npy')
+fig=plt.figure()
+
+for num,data in enumerate(test_data[:12]):
+    # cat: [1,0]
+    # dog: [0,1]
+    
+    img_num = data[1]
+    img_data = data[0]
+    
+    y = fig.add_subplot(3,4,num+1)
+    orig = img_data
+    data = img_data.reshape(IMG_SIZE,IMG_SIZE,1)
+    #model_out = model.predict([data])[0]
+    model_out = tf_model.predict([data])[0]
+    
+    if np.argmax(model_out) == 1: str_label='Dog'
+    else: str_label='Cat'
+        
+    y.imshow(orig,cmap='gray')
+    plt.title(str_label)
+    y.axes.get_xaxis().set_visible(False)
+    y.axes.get_yaxis().set_visible(False)
+plt.show()
